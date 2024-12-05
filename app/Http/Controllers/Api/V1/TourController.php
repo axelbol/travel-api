b693f08e7088bbee77d0de429f942e76686438aa
axelbol <axel_lp_alvarado@hotmail.com>
2024-12-04 23:39:09 -0400

    tours filter & order

--- app/Http/Controllers/Api/V1/TourController.php
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\TourListRequest;
 use App\Http\Resources\TourResource;
 use App\Models\Tour;
 use App\Models\Travel;
@@ -10,7 +11,7 @@ use Illuminate\Http\Request;
 
 class TourController extends Controller
 {
-    public function index(Travel $travel, Request $request)
+    public function index(Travel $travel, TourListRequest $request)
     {
         $tours = Tour::where('travel_id', $travel->id)
             ->when($request->dateFrom, function ($query) use ($request) {
@@ -24,6 +25,9 @@ class TourController extends Controller
             })
             ->when($request->priceTo, function ($query) use ($request) {
                 $query->where('price', '<=', $request->priceTo * 100);
+            })
+            ->when($request->sortBy && $request->sortOrder, function ($query) use ($request) {
+                $query->orderBy($request->sortBy, $request->sortOrder);
             })
             ->orderBy('starting_date')
             ->paginate();
